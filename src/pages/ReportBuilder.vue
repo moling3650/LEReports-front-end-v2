e10a74262cb4385cb157c63435fd6373bdb2ab22
moling <365024424@qq.com>
2018-04-14 11:41:09 +0800

    添加两个页面，并配置路由

---
+++ src/pages/ReportBuilder.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="ReportBuilder">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ReportBuilder',
+  created () {
+    window.document.title = '报表编辑器'
+  }
+}
+</script>
