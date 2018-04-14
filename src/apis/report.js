0b054d106480f2222e7cf3e56f9eba9b93e3efc4
moling <365024424@qq.com>
2018-04-14 15:42:46 +0800

    添加删除report的API

--- src/apis/report.js
+++ src/apis/report.js
@@ -4,6 +4,11 @@ function fetchReports () {
   return axios.get('/DataAPI/Commom.ashx?ActType=GetReports').then(res => res.data)
 }
 
+function deleteReportByCode (code) {
+  return axios.post('/DataAPI/ReportData.ashx?method=deleteReport', {queryParams: { report_code: code }})
+}
+
 export {
-  fetchReports
+  fetchReports,
+  deleteReportByCode
 }
