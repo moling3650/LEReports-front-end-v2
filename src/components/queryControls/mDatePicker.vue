22800b967c5d0e8b1adb547733be9bf0ca03761a
moling <365024424@qq.com>
2018-04-14 10:00:41 +0800

    添加mDatePicker查询控件组件

---
+++ src/components/queryControls/mDatePicker.vue
@@ -0,0 +1,42 @@
+<template>
+  <el-date-picker
+    v-model="value"
+    type="datetimerange"
+    range-separator=" 至 "
+    start-placeholder="开始日期"
+    end-placeholder="结束日期"
+    @change="handleChange">
+  </el-date-picker>
+</template>
+
+<script>
+export default {
+  name: 'mDatePicker',
+  props: {
+    label: {
+      type: String
+    },
+    ex: {
+      type: String,
+      default: '31'
+    }
+  },
+  data () {
+    return {
+      value: null
+    }
+  },
+  methods: {
+    handleChange () {
+      if (this.value && this.value.length) {
+        const days = parseInt(this.ex) || 31
+        const maxDate = new Date(this.value[0].getTime() + days * 24 * 3600000)
+        if (maxDate < this.value[1]) {
+          this.$message({ showClose: true, message: `最多只能查询${days}天的数据` })
+          this.$set(this.value, 1, maxDate)
+        }
+      }
+    }
+  }
+}
+</script>
