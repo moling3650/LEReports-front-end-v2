83a59eccf727b5273ff85d622494286b6eb5ce3d
moling <365024424@qq.com>
2018-04-14 10:00:02 +0800

    添加mInputMultiValue查询控件组件

---
+++ src/components/queryControls/mInputMultiValue.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-input v-model.trim="rawValue" clearable :placeholder="`请输入一个或多个${label}`" @keyup.enter.native="$emit('enter')"/>
+</template>
+
+<script>
+export default {
+  name: 'mInputMultiValue',
+  props: {
+    label: {
+      type: String,
+      default: '数值'
+    }
+  },
+  data () {
+    return {
+      rawValue: ''
+    }
+  },
+  computed: {
+    value () {
+      return this.rawValue && this.rawValue.split(/[\s,]+/)
+    }
+  }
+}
+</script>
