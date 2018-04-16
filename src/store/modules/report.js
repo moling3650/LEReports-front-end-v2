0c22ba01d27b4a70c8b21838b5d9e5e4b46aff17
moling <365024424@qq.com>
2018-04-16 13:32:39 +0800

    更新store的report模块

--- src/store/modules/report.js
+++ src/store/modules/report.js
@@ -1,4 +1,4 @@
-import { fetchReports, deleteReportByCode } from '@/apis/report'
+import api from '@/apis/report'
 
 // initial state
 const state = {
@@ -7,16 +7,24 @@ const state = {
 
 // getters
 const getters = {
-  reportOptions: state => state.all.map(r => ({ label: r.report_name, value: r.report_code }))
+  reportOptions: state => state.all.map(r => ({ label: r.report_name, value: r.report_code })),
+  getReportByCode: state => code => Object.assign({query_type: 1}, state.all.find(r => r.report_code === code))
 }
 
 // actions
 const actions = {
   fetchReports ({ commit }) {
-    return fetchReports().then(reports => commit('setReports', reports))
+    return api.fetchReports().then(reports => commit('setReports', reports))
   },
   deleteReportByCode ({ commit }, code) {
-    return deleteReportByCode(code).then(() => commit('removeReportByCode', code))
+    return api.deleteReportByCode(code).then(() => commit('removeReportByCode', code))
+  },
+  saveReport ({ commit }, report) {
+    report.query_sql = `SELECT * FROM ${report.report_code}`
+    return api.saveReport(report).then(() => commit('updateReport', report))
+  },
+  validReportCode ({ commit }, code) {
+    return api.validReportCode(code)
   }
 }
 
@@ -28,6 +36,10 @@ const mutations = {
   removeReportByCode (state, code) {
     const index = state.all.findIndex(r => r.report_code === code)
     ~index && state.all.splice(index, 1)
+  },
+  updateReport (state, report) {
+    const index = state.all.findIndex(r => r.report_code === report.report_code)
+    state.all.splice(~index ? index : state.all.length, 1, report)
   }
 }
 
