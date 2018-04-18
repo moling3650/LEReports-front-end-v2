a06805efb326dea802133419c8d434008c9d7abf
moling <365024424@qq.com>
2018-04-18 18:50:16 +0800

    添加查询报表的API

--- src/apis/index.js
+++ src/apis/index.js
@@ -4,6 +4,11 @@ function fetchQueryControls () {
   return axios.get('/DataAPI/Commom.ashx?ActType=GetQueryControls').then(res => res.data)
 }
 
+function fetchData (payload) {
+  return axios.post('/DataAPI/ReportData.ashx?method=FetchData', payload).then(res => res.data)
+}
+
 export default {
-  fetchQueryControls
+  fetchQueryControls,
+  fetchData
 }
