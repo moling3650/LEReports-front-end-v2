7b1cfed1a01074615072a14150c2f9fedc08aa29
moling <365024424@qq.com>
2018-04-19 16:25:03 +0800

    添加获取选择项的API

--- src/apis/index.js
+++ src/apis/index.js
@@ -8,7 +8,12 @@ function fetchData (payload) {
   return axios.post('/DataAPI/ReportData.ashx?method=FetchData', payload).then(res => res.data)
 }
 
+function fetchOptions (sql) {
+  return axios.get(`/DataAPI/Commom.ashx?ActType=GetOptions&sql=${sql}`).then(res => res.data)
+}
+
 export default {
   fetchQueryControls,
-  fetchData
+  fetchData,
+  fetchOptions
 }
