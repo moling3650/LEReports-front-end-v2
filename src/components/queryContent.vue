3942f07fd5a5d790da9a4d25c2a8a56cd3eec658
moling <365024424@qq.com>
2018-04-27 14:35:35 +0800

    修改queryContent组件的结构

--- src/components/queryContent.vue
+++ src/components/queryContent.vue
@@ -1,40 +1,50 @@
 <template>
   <div id="queryContent">
-    <el-button-group :style="{ float: 'right', margin: '5px' }">
+    <el-button-group :style="{ float: 'right', margin: '-30px 5px 0' }">
       <el-button size="mini" type="primary" @click="handleSelectFields">筛选字段</el-button>
       <el-button size="mini" type="primary" @click="handleExport">导出</el-button>
     </el-button-group>
-    <div class="z-table">
-      <div class="table-wrap" :style="{ height: (pageSize + 2) * 36 + 'px'}">
-        <el-table v-loading="loading"
-          :data="tableData" border stripe size="mini"
-          :header-cell-style="{backgroundColor: '#409eff', color: '#fff'}"
-          element-loading-text="拼命加载中"
-          element-loading-spinner="el-icon-loading"
-          element-loading-background="rgba(0, 0, 0, 0.8)"
-          style="width: 100%"
-          tooltip-effect="dark"
-        >
-          <el-table-column v-for="f in fields"
-            v-if="f.state"
-            :key="f.prop"
-            :prop="f.prop"
-            :label="f.label"
-            :width="f.width"
-            :align="f.align"
-            show-overflow-tooltip
-          />
-        </el-table>
-      </div>
-      <el-pagination
-      v-if="data.length"
-      background
-      style="position: fixed;bottom: 20px; right: 10px"
-      layout="total, prev, pager, next"
-      :total="data.length"
-      :page-size="pageSize"
-      :current-page.sync="index"/>
-    </div>
+    <el-tabs type="border-card" style="margin-top: 30px;" @tab-click="handleTabClick">
+      <el-tab-pane label="数据表格">
+        <div class="z-table tab-content">
+          <div class="table-wrap" :style="{ height: (pageSize + 2) * 36 + 50 + 'px'}">
+            <el-table v-loading="loading"
+              :data="tableData" border stripe size="mini"
+              :header-cell-style="{backgroundColor: '#409eff', color: '#fff'}"
+              element-loading-text="拼命加载中"
+              element-loading-spinner="el-icon-loading"
+              element-loading-background="rgba(0, 0, 0, 0.8)"
+              style="width: 100%"
+              tooltip-effect="dark"
+            >
+              <el-table-column v-for="f in fields"
+                v-if="f.state"
+                :key="f.prop"
+                :prop="f.prop"
+                :label="f.label"
+                :width="f.width"
+                :align="f.align"
+                show-overflow-tooltip
+              />
+            </el-table>
+          </div>
+          <el-pagination
+          v-if="data.length"
+          background
+          style="position: absolute;bottom: 5px; right: 10px"
+          layout="total, prev, pager, next"
+          :total="data.length"
+          :page-size="pageSize"
+          :current-page.sync="index"/>
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="折线图">
+        <div class="tab-content">
+          <zLine :data="data" label="process_name" :values="[{code: 'type_id', name: '类型ID'}]"/>
+        </div>
+      </el-tab-pane>
+    </el-tabs>
+
     <fieldsForm ref="fieldsDialog"/>
   </div>
 </template>
@@ -42,11 +52,13 @@
 <script>
 import { exportXlsx } from '@/lib/exportData'
 import fieldsForm from '@/components/Forms/fieldsForm'
+import zLine from '@/components/Chart/zLine'
 
 export default {
   name: 'queryContent',
   components: {
-    fieldsForm
+    fieldsForm,
+    zLine
   },
   props: {
     loading: {
@@ -76,6 +88,11 @@ export default {
     }
   },
   methods: {
+    handleTabClick (tab) {
+      this.$nextTick(() => {
+        tab.$children.filter(c => c.chart && c.chart.resize).map(c => c.chart.resize())
+      })
+    },
     handleSelectFields () {
       this.$refs.fieldsDialog.open()
     },
@@ -96,5 +113,11 @@ export default {
 #queryContent {
   width: 98%;
   margin: 5px 1%;
+}
+
+.tab-content {
+  position: relative;
+  width: 100%;
+  height: 480px;
 }
 </style>
