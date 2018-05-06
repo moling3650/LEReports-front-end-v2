6e5fcff1c0d76de3c11bffe9d27e6e4895f3fe01
moling <365024424@qq.com>
2018-05-06 16:24:38 +0800

    添加SfcProcessReport页面

---
+++ src/pages/SfcProcessReport.vue
@@ -0,0 +1,93 @@
+<template>
+  <div id="SfcProcessReport">
+    <el-row :gutter="20">
+      <el-col :span="6">
+        <el-input v-model.trim="sfc" @keyup.enter.native="search"/>
+      </el-col>
+      <el-col :span="2">
+        <el-button type="primary" @click="search">查询</el-button>
+      </el-col>
+    </el-row>
+    <div class="table-wrap">
+      <el-table :data="tableData" border stripe size="mini" height="500"
+        :header-cell-style="{backgroundColor: '#409eff', color: '#fff'}"
+        @row-dblclick="handleDbclick">
+        <el-table-column v-for="f in tableFields" :key="f" :prop="f" :label="f"/>
+      </el-table>
+    </div>
+    <el-dialog :title="processName" :visible.sync="dialogVisible" :fullscreen="true">
+      <el-table :data="subTableData" border stripe size="mini" height="600"
+        :header-cell-style="{backgroundColor: '#409eff', color: '#fff'}">
+        <el-table-column  v-for="f in subTableFileds" :key="f.prop" :prop="f.prop" :label="f.label"/>
+      </el-table>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import api from '../apis/sfcDcProcess'
+
+export default {
+  name: 'SfcProcessReport',
+  data () {
+    return {
+      dialogVisible: false,
+      sfc: '',
+      processName: '',
+      tableFields: ['工单', '条码'],
+      tableData: [],
+      subTableFileds: [],
+      subTableData: []
+    }
+  },
+  methods: {
+    search () {
+      api.fetchDataBySFC(this.sfc).then(data => {
+        if (data.length) {
+          this.tableFields = Object.keys(data[0])
+        }
+        this.tableData = data
+      })
+    },
+    handleDbclick ({'条码': sfc, '工序': processName}) {
+      if (this.processName === processName) {
+        this.dialogVisible = true
+        return
+      }
+      this.processName = processName
+      Promise.all([api.fetchFields(processName), api.fetchProcessDataBySFC(sfc, processName)])
+        .then(([fields, data]) => {
+          if (fields.length) {
+            this.subTableFileds = fields.filter(f => f.state).map(f => {
+              return {
+                prop: f.prop,
+                label: f.label
+              }
+            })
+          } else if (data.length) {
+            this.subTableFileds = Object.keys(data[0]).map(key => {
+              return {
+                prop: key,
+                label: key
+              }
+            })
+          }
+          this.subTableData = data
+          this.dialogVisible = true
+        })
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+#SfcProcessReport {
+  margin: 5px;
+}
+
+.table-wrap {
+  /*height: 500px;*/
+  margin-top: 5px;
+  /*overflow-y: scroll;*/
+}
+</style>
