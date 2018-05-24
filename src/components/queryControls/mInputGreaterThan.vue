025b8f02e6993bf265ce6ec3191414fda8b581bc
moling <365024424@qq.com>
2018-05-24 15:09:43 +0800

    添加mInputGreaterThan查询组件

---
+++ src/components/queryControls/mInputGreaterThan.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-input style="vertical-align: top;" v-model.number="value" clearable @keyup.enter.native="$emit('enter')">
+    <template slot="prepend">{{ label }} &gt;= </template>
+  </el-input>
+</template>
+
+<script>
+export default {
+  name: 'mInputGreaterThan',
+  props: {
+    label: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      value: ''
+    }
+  }
+}
+</script>
